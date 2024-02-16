f29db02322ac8a712d20373f4bbaf580f2157e81
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-16 14:41:36 -0800

    install chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+// import from react, 3rd party libraries and then our own import statements
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
