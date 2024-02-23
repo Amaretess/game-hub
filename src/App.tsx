062471b9d7c09c55373f188ee9a738058b2c2386
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-22 16:15:36 -0800

    chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 const App = () => {
   return (
-    <ButtonGroup gap="4">
-      <Button colorScheme='blue'>Button</Button>
-      <Button colorScheme='blue'>Button</Button>
-      <Button colorScheme='blue'>Button</Button>
-    </ButtonGroup>
+    <>
+      {/* "1st row 1st column 1st row 2nd column 1st row 3rd column"  "2nd row 1st col 2nd row 2nd col, 2nd row 3rd col" */}
+      <Grid templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+      }} >
+        <GridItem area="nav" bg="coral">
+          nav
+        </GridItem>
+        {/* will SHOW on large screens and above */}
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            aside
+          </GridItem>
+
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          main
+        </GridItem>
+      </Grid>
+    </>
   )
 }
 
